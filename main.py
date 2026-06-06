ee15718679d3af7205e1b1526f41053745f4a555
Juanes122 <rinconatehortuajuanesteban@gmail.com>
2026-06-06 10:53:38 -0500

    Actualizar endpoints de FastAPI

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException  
+from pydantic import BaseModel  
 from typing import List
 
 app = FastAPI()
